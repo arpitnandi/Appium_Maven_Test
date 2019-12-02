524d2d79826764577827df4ee33944dbb1d8276e
arpitnandi1 <arpitnandi1@gmail.com>
2019-12-02 17:01:41 +0530

    Updating BaseClass.java

--- Android.Test/src/test/java/project/Utils/BaseClass.java
+++ Android.Test/src/test/java/project/Utils/BaseClass.java
@@ -5,6 +5,7 @@ import java.net.URL;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -37,5 +38,73 @@ public class BaseClass
 		A = new Actions( D );
 		
 		return D;
+	}
+
+	public WebElement element( String By, String locator )
+	{
+		WebElement E = null;
+
+		if( By.endsWith( "accessibilityId" ) )
+			E = D.findElementByAccessibilityId( locator );
+		
+		if( By.endsWith( "id" ) )
+			E = D.findElementById( locator );
+		
+		if( By.endsWith( "class" ) )
+			E = D.findElementByClassName( locator );
+		
+		if( By.endsWith( "cssSelector" ) )
+			E = D.findElementByCssSelector( locator );
+
+		if( By.endsWith( "linkText" ) )
+			E = D.findElementByLinkText( locator );
+		
+		if( By.endsWith( "name" ) )
+			E = D.findElementByName( locator );
+
+		if( By.endsWith( "partialLinkText" ) )
+			E = D.findElementByPartialLinkText( locator );
+
+		if( By.endsWith( "tagName" ) )
+			E = D.findElementByTagName( locator );
+
+		if( By.endsWith( "xPath" ) )
+			E = D.findElementByXPath( locator );
+		
+		return E;
+	}
+	
+	public List<MobileElement> elements( String By, String locator )
+	{
+		List<MobileElement> Es = new ArrayList<MobileElement> ();
+
+		if( By.endsWith( "accessibilityId" ) )
+			Es = D.findElementsByAccessibilityId( locator );
+		
+		if( By.endsWith( "id" ) )
+			Es = D.findElementsById( locator );
+		
+		if( By.endsWith( "class" ) )
+			Es = D.findElementsByClassName( locator );
+		
+		if( By.endsWith( "cssSelector" ) )
+			Es = D.findElementsByCssSelector( locator );
+
+		if( By.endsWith( "linkText" ) )
+			Es = D.findElementsByLinkText( locator );
+		
+		if( By.endsWith( "name" ) )
+			Es = D.findElementsByName( locator );
+
+		if( By.endsWith( "partialLinkText" ) )
+			Es = D.findElementsByPartialLinkText( locator );
+
+		if( By.endsWith( "tagName" ) )
+			Es = D.findElementsByTagName( locator );
+
+		if( By.endsWith( "xPath" ) )
+			Es = D.findElementsByXPath( locator );
+		
+		return Es;
 	}
 }
